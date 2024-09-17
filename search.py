d6a6c09bdeff287d251ae6258669f8e97e71d911
jwcowand <jwcowand@gmail.com>
2024-09-17 10:11:57 -0400

    search function implement ENPM611

--- search.py
+++ search.py
@@ -5,4 +5,7 @@ def search_json(json_data, search_string):
     # Place your search code here
     # you will have to loop through the json_data file you created earlier
     # finally you can store the match in the result list and return it
+    #data = json.load(json_data)
+    
+    results = [lines for lines in json_data if any(search_string in str(value)for value in lines.values() or lines.objects())]
     return results
